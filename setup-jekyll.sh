98b78b12769c2ed415ed0270242290c4dc5967d5
Owner <thedayisntgray@gmail.com>
2020-08-03 22:47:29 -0500

    Append to end of bashrc file

--- setup-jekyll.sh
+++ setup-jekyll.sh
@@ -83,8 +83,9 @@ sudo apt -y install nginx
 sudo ufw allow 'Nginx HTTP'
 sudo apt -y install make build-essential ruby ruby-dev
 
-sudo echo 'export GEM_HOME=$HOME/gems' >> .bashrc
-sudo echo 'export PATH=$HOME/gems/bin:$PATH' >> .bashrc
+echo "export GEM_HOME=$HOME/gems" | sudo tee .bashrc
+echo "export PATH=$HOME/gems/bin:$PATH" | sudo tee .bashrc
+
 source ~/.bashrc
 gem install jekyll bundler
 jekyll new client-site
