eacd804f9ad7e242b75d1eba1c9f38018f0a2514
Owner <thedayisntgray@gmail.com>
2020-08-08 14:06:33 -0500

    Run gen install Jekyll and bundler as root

--- curl-for-setup-script.sh
+++ curl-for-setup-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl -L https://raw.githubusercontent.com/wgraydev/public-files/master/setup-jekyll.sh -o /tmp/initial_setup.sh
-bash /tmp/initial_setup.sh
+bash /tmp/initial_setup.sh | tee output.txt
